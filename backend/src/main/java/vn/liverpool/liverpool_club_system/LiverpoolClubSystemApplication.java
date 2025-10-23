0a00e5b41a355013cd06ce194b6535212656942c
Tín <Trungtin0701200@gmail.com>
2025-10-23 14:25:42 +0700

    Tạo base dự án

---
+++ backend/src/main/java/vn/liverpool/liverpool_club_system/LiverpoolClubSystemApplication.java
@@ -0,0 +1,13 @@
+package vn.liverpool.liverpool_club_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiverpoolClubSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiverpoolClubSystemApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/vn/liverpool/liverpool_club_system/LiverpoolClubSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.liverpool.liverpool_club_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiverpoolClubSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
