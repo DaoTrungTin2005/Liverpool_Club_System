84fe17464956dc23138c3e50cad6d3c41e47947f
Tín <Trungtin0701200@gmail.com>
2025-10-23 15:21:30 +0700

    Tạo RegisterDTO

---
+++ backend/src/main/java/vn/liverpool/domain/dto/RegisterDTO.java
@@ -0,0 +1,22 @@
+package vn.liverpool.domain.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterDTO {
+    @NotBlank(message = "Full name is required")
+    private String fullname;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+}
