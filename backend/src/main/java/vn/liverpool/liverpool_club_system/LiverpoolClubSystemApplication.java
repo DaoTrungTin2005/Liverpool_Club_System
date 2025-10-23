d2f4a474dcc7145a320cb63891e2afaaf43cfce7
Tín <Trungtin0701200@gmail.com>
2025-10-23 15:15:36 +0700

    Cấu hình main cho nhận được các entity

--- backend/src/main/java/vn/liverpool/liverpool_club_system/LiverpoolClubSystemApplication.java
+++ backend/src/main/java/vn/liverpool/liverpool_club_system/LiverpoolClubSystemApplication.java
@@ -2,8 +2,10 @@ package vn.liverpool.liverpool_club_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "vn.liverpool.domain")
 public class LiverpoolClubSystemApplication {
 
 	public static void main(String[] args) {
