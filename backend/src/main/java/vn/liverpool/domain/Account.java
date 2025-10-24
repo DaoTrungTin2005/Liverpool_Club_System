e5576ed16dd2c1f65cf4cc129c5bd10212c2136d
Tín <Trungtin0701200@gmail.com>
2025-10-24 12:20:09 +0700

    Thêm createdAt và updatedAt ở Entity

--- backend/src/main/java/vn/liverpool/domain/Account.java
+++ backend/src/main/java/vn/liverpool/domain/Account.java
@@ -1,5 +1,7 @@
 package vn.liverpool.domain;
 
+import java.time.Instant;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -7,6 +9,8 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -32,4 +36,24 @@ public class Account {
     @ManyToOne
     @JoinColumn(name = "role_id")
     private Role role;
+
+    @Column(name = "created_at", updatable = false) // ko cho sửa
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt; // lưu thời điểm bản ghi chỉnh sửa gần nhất
+
+    // tự chạy khi tạo mới
+    @PrePersist
+    protected void onCreate() {
+        Instant now = Instant.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    // tự chạy khi cập nhật
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = Instant.now(); // khi update, chỉ updatedAt được cập nhật = thời gian hiện tại.
+    }
 }
